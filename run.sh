c9a18adc2fb081f1f66dbef652e01e5c9940efc6
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-12-14 01:37:51 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 wget https://codeload.github.com/aashath0317/mirror-leech-telegram-bot/zip/refs/heads/master
 unzip master
 mv mirror-leech-telegram-bot-master/* /usr/src/app/
+pip3 install --no-cache-dir -r requirements.txt
 python3 update.py && python3 -m bot
